3f6d5a8645c6b739fb3ef4c4516b3a554bd4132d
BRIAN K. <140699437+rotich-brian@users.noreply.github.com>
2024-10-15 16:23:04 +0300

    Update script.py

--- scripts/script.py
+++ scripts/script.py
@@ -44,8 +44,10 @@ try:
 
     options = Options()
     options.headless = True
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    
     options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
     options.proxy = proxy_settings
