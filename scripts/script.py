17382d7774f7a2714e2e58a5cdb86d70188b1ef6
BRIAN K. <140699437+rotich-brian@users.noreply.github.com>
2024-10-15 17:36:05 +0300

    Update script.py

--- scripts/script.py
+++ scripts/script.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from browsermobproxy import Server
 
 # Set up logging
